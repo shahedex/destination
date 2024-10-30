668b833ab65a939312957de6e99b962058989798
shahedex <shahed739@gmail.com>
2024-10-30 13:46:07 +0000

    checking folder content copy

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask,jsonify,request
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello world from CI/CD from jenkins webhook'
+
+@app.route('/health')
+def hello_world():
+    return 'Status: Healthy :-D'
+    
+@app.route('/route-sync')
+def hello_world():
+    return 'Status: Healthy route sync done :-D'
+
+@app.route('/reverser',methods = ['POST'])
+def reverser():
+    num = request.get_json().get("num")
+    num = int(num[len(num)::-1])
+    return jsonify({"num":num})
+
+
+@app.route('/summation',methods = ['POST'])
+def summation():
+    num = request.get_json().get("num")
+    sum = 0
+    for i in num:
+        sum += int(i)
+
+    return jsonify({"sum":sum})
+
+if __name__ == "__main__":
+     app.run()
